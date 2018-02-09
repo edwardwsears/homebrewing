ceb17b4f7d2a7ff340e9ac1b13bca7161c2c25e3
Edward Sears <edwardwsears@gmail.com>
2018-02-08 20:13:55 -0800

    Adding first brewAI model

--- ai_brewmaster/scrape_beerxml.py
+++ ai_brewmaster/scrape_beerxml.py
@@ -9,32 +9,46 @@ import re
 # Usage: %> python scrape_beerxml.py
 #
 
-# Index range for brewersfiend crawling
-start_index = 1633
-end_index   = 16000
+# Index range for brewersfiend crawling, change file every 10000 indexes
+start_index = 110000
+end_index   = 120000
 
 tot_count = 1
 
-# Output file location
-output_file = open("data/beerxml_recipes.txt", "w")
+while (True):
+    # Output file location
+    output_file = open("data/beerxml_recipes_"+str(start_index)+"_"+str(end_index)+".txt", "w")
 
-for index in range(start_index, end_index):
-    url = 'https://www.brewersfriend.com/homebrew/recipe/beerxml1.0/'+str(index)
-    page = requests.get(url)
-    page_text = page.text
-    if (page):
-        # only use pages that successfully found a recipe
-        if (re.search("<RECIPE>", page.text)):
-            print "Downloaded BeerXML at index: " + str(index) + ", total: " + str(tot_count) + "\n"
-            # add a space around <> tags so that they are split into independent words in train.py
-            page_text = re.sub("<", " <", page_text)
-            page_text = re.sub(">", "> ", page_text)
-            page_text = re.sub("&#13;", "\n", page_text)
+    for index in range(start_index, end_index):
+        url = 'https://www.brewersfriend.com/homebrew/recipe/beerxml1.0/'+str(index)
+        try:
+            page = requests.get(url)
+        except:
+            print "Exeption thrown"
+            time.sleep(10)
+            continue
+        page_text = page.text
+        if (page):
+            # only use pages that successfully found a recipe
+            if (re.search("<RECIPE>", page.text)):
+                print "Downloaded BeerXML at index: " + str(index) + ", total: " + str(tot_count) + "\n"
+                # add a space around <> tags so that they are split into independent words in train.py
+                page_text = re.sub("<", " <", page_text)
+                page_text = re.sub(">", "> ", page_text)
+                # remove special character
+                page_text = re.sub("&#13;", "\n", page_text)
+                # move style to first block after recipe
+                matchObj = re.match( r'(.*)<RECIPE>(.*)<STYLE>(.*)</STYLE>(.*)', page_text, re.S)
+                page_text = matchObj.group(1) + "<RECIPE> \n   <STYLE>" + matchObj.group(3) + "</STYLE>" + matchObj.group(2) + matchObj.group(4)
 
-            output_file.write(page_text.encode('ascii', 'ignore'))
-            tot_count = tot_count + 1;
+                output_file.write(page_text.encode('utf-8', 'ignore'))
+                tot_count = tot_count + 1;
 
-    # only query website once per second
-    time.sleep(1)
+        # only query website once per 2 seconds
+        time.sleep(2)
 
-output_file.close()
+    output_file.close()
+
+    # update start & end indexes
+    start_index = start_index + 10000
+    end_index   = end_index + 10000
